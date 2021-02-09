bc4bd12388562c58389ab7c11d4c395c436536b2
JosephASandoval <josephasandoval17@gmail.com>
2021-02-09 15:51:23 -0800

    Rendered signup page

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import SignupFormContainer from "./session/signup_form_container";
+import { Route } from "react-router-dom";
+
+export default () => (
+  <div>
+    <Route path="/signup" component={SignupFormContainer} />
+  </div>
+);
