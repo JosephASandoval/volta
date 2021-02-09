8692f9ddcb4108fe96e4dc8a8dc0b036407370a0
JosephASandoval <josephasandoval17@gmail.com>
2021-02-09 14:34:44 -0800

    Create a session util file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   helper_method :current_user, :logged_in?
 
   private
 
   def current_user
+    return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
   def require_login
-    redirect_to new_session_url unless logged_in?
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
+    end
+    # redirect_to new_session_url unless logged_in?
   end
 
   def logged_in?
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+u1 = User.create!(
+  username: 'user1',
+  password: 'password',
+  email: 'user1@gmail.com',
+  first_name: 'user',
+  last_name: '1'
+)
+
+u2 = User.create!(
+  username: 'user2',
+  password: 'password',
+  email: 'user2@gmail.com',
+  first_name: 'user',
+  last_name: '2'
+)
