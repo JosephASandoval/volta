ff423e5ad73e0b4ef9676867eb058c7d64424dd6
JosephASandoval <josephasandoval17@gmail.com>
2021-02-10 10:24:07 -0800

    Worked on some css

---
+++ frontend/components/header/header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../../../app/assets/stylesheets/components/_header.scss";
+import TeslaLogo from "../../../app/assets/images/teslaLogoSmall.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={TeslaLogo} alt="Tesla Logo" />
+      </div>
+
+      <div className="header__center">
+        <p>Model S</p>
+        <p>Model 3</p>
+        <p>Model X</p>
+        <p>Model Y</p>
+        <p>Solar Roof</p>
+        <p>Solar Panel</p>
+      </div>
+
+      <div className="header__right">
+        <p>Shop</p>
+        <p>Tesla Account</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
