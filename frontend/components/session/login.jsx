9fa578890e1d545091e2de2521be780767537868
JosephASandoval <josephasandoval17@gmail.com>
2021-02-10 07:13:29 -0800

    Fixed login issue

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -19,7 +19,7 @@ class Login extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.login(this.state).then(() => this.props.history.push("/"));
+    this.props.loginUser(this.state).then(() => this.props.history.push("/"));
   }
 
   render() {
