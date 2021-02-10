6e27459a5216d219bc10a025dc90b41cf8697fe5
JosephASandoval <josephasandoval17@gmail.com>
2021-02-10 08:39:58 -0800

    Updated

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Home from "./home/home";
-import NavBarContainer from "./nav_bar/nav_bar_container";
+import GreetingContainer from "./greeting/greeting_container";
 import SignupContainer from "./session/signup_container";
 import LoginContainer from "./session/login_container";
 import { Route, Link } from "react-router-dom";
@@ -8,8 +7,7 @@ import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
 const App = () => (
   <div>
-    <Route path="/" component={NavBarContainer} />
-    <Route exact path="/" component={Home} />
+    <Route path="/" component={GreetingContainer} />
     <Route path="/signup" component={SignupContainer} />
     <Route path="/login" component={LoginContainer} />
   </div>
--- frontend/components/home/home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default () => (
-  <div className="home">
-    <h1>Welcome to Tesla!</h1>
-  </div>
-);
