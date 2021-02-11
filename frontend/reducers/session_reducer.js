574b44255affa64fead4c1c9ea4162a29fe3dcf1
JosephASandoval <josephasandoval17@gmail.com>
2021-02-11 14:22:27 -0800

    Updated state

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -3,18 +3,17 @@ import {
   LOGOUT_CURRENT_USER,
 } from "../actions/session_actions";
 
-const _nullSession = {
-  currentUser: null,
-};
+const _nullUser = Object.freeze({
+  id: null,
+});
 
-const sessionReducer = (oldState = _nullSession, action) => {
+const sessionReducer = (oldState = _nullUser, action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      const currentUser = action.user;
-      return Object.assign({}, { currentUser });
+      return { id: action.currentUser.id };
     case LOGOUT_CURRENT_USER:
-      return _nullSession;
+      return _nullUser;
     default:
       return oldState;
   }
