393bbbda695e328fc9618f569dcc1892b1ddfbad
JosephASandoval <josephasandoval17@gmail.com>
2021-02-11 14:42:29 -0800

    Updated

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -11,7 +11,7 @@ const sessionReducer = (oldState = _nullUser, action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return { id: action.currentUser.id };
+      return { currentUserId: action.currentUser.id };
     case LOGOUT_CURRENT_USER:
       return _nullUser;
     default:
