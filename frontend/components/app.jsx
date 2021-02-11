753c1d4109d3aa3adf27060ba67d357750d8a2c1
JosephASandoval <josephasandoval17@gmail.com>
2021-02-11 07:58:21 -0800

    Updated

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import GreetingContainer from "./greeting/greeting_container";
 import SignupContainer from "./session/signup_container";
 import LoginContainer from "./session/login_container";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import Index from "./homepage/index";
 
 const App = () => (
   <div>
     <Route exact path="/" component={Index} />
+    <AuthRoute path="/signup" component={SignupContainer} />
+    <AuthRoute path="/login" component={LoginContainer} />
     <Route path="/greeting" component={GreetingContainer} />
-    <Route path="/signup" component={SignupContainer} />
-    <Route path="/login" component={LoginContainer} />
   </div>
 );
 
