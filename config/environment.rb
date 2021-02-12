640e6a92fde670f943e2a5b493f8c4f61930e4de
JosephASandoval <josephasandoval17@gmail.com>
2021-02-12 14:58:18 -0800

    Update state with camel case

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# returns the Jbuilder object with camelCased keys
+Jbuilder.key_format camelize: :lower
