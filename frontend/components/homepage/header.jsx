f25ec2c651eb2aec5c02ed780d9d861134a18738
JosephASandoval <josephasandoval17@gmail.com>
2021-02-11 20:25:17 -0800

    Updated user auth

--- frontend/components/homepage/header.jsx
+++ frontend/components/homepage/header.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => {
-  return (
+const Header = ({currentUser}) => {
+  return currentUser ? (
+    <div className="header">
+      <div className="header_logo">
+        <Link to="/">
+          <img src={window.teslaLogoSmallURL} alt="Tesla Logo" />
+        </Link>
+      </div>
+
+      <div className="header_center">
+        <p>Model S</p>
+        <p>Model 3</p>
+        <p>Model X</p>
+        <p>Model Y</p>
+        <p>Solar Roof</p>
+        <p>Solar Panels</p>
+      </div>
+
+      <div className="header_right">
+        <p>Shop</p>
+        <p>
+          <Link to="/greeting">Tesla Account</Link>
+        </p>
+        <p>
+          <i className="fas fa-bars"></i>
+        </p>
+      </div>
+    </div>
+  ) : (
     <div className="header">
       <div className="header_logo">
         <Link to="/">
