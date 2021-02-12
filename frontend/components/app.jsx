8e0f5682c1b436bf55f0bf7009ffcd87b86c42c7
JosephASandoval <josephasandoval17@gmail.com>
2021-02-12 08:58:59 -0800

    Added a page not found

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,14 +7,18 @@ import Index from "./homepage/index";
 import GreetingContainer from "./greeting/greeting_container";
 import SignupContainer from "./session/signup_container";
 import LoginContainer from "./session/login_container";
+import NoMatch from "./no_match/no_match"
 
 
 const App = () => (
   <div>
-    <Route exact path="/" component={Index} />
-    <AuthRoute path="/signup" component={SignupContainer} />
-    <AuthRoute path="/login" component={LoginContainer} />
-    <Route path="/greeting" component={GreetingContainer} />
+    <Switch>
+      <Route exact path="/" component={Index} />
+      <AuthRoute path="/signup" component={SignupContainer} />
+      <AuthRoute path="/login" component={LoginContainer} />
+      <Route path="/greeting" component={GreetingContainer} />
+      <Route path="*" component={NoMatch}/>
+    </Switch>
   </div>
 );
 
