60563c33f82c84d10ee2d55ceab73c819de49443
JosephASandoval <josephasandoval17@gmail.com>
2021-02-13 16:04:39 -0800

    Updated sat

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -9,16 +9,22 @@ import SignupContainer from "./session/signup_container";
 import LoginContainer from "./session/login_container";
 import NotFound from "./notfound/notfound";
 
-const App = () => (
-  <div>
-    <Switch>
-      <Route exact path="/" component={Splash} />
-      <AuthRoute path="/signup" component={SignupContainer} />
-      <AuthRoute path="/login" component={LoginContainer} />
-      <Route path="/userProfile" component={UserProfileContainer} />
-      <Route path="*" component={NotFound} />
-    </Switch>
-  </div>
-);
+const App = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Splash} />
+        <AuthRoute path="/signup" component={SignupContainer} />
+        <AuthRoute path="/login" component={LoginContainer} />
+        <Route path="/userProfile" component={UserProfileContainer} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
