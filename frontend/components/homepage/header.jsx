0b99867dc40f5e4712b95ccc439625784e87fda3
JosephASandoval <josephasandoval17@gmail.com>
2021-02-15 19:05:23 -0800

    Refactored header logic

--- frontend/components/homepage/header.jsx
+++ frontend/components/homepage/header.jsx
@@ -4,7 +4,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 import CloseIcon from "@material-ui/icons/Close";
 
 const Header = ({ currentUser, isMenuOpen, setIsMenuOpen }) => {
-  return currentUser ? (
+  return (
     <div className="header">
       <div className="header__logo">
         <Link to="/">
@@ -25,44 +25,22 @@ const Header = ({ currentUser, isMenuOpen, setIsMenuOpen }) => {
         <Link to="/" className={isMenuOpen ? "header__link--hidden" : ""}>
           Shop
         </Link>
-        <Link
-          to="/userProfile"
-          className={isMenuOpen ? "header__link--hidden" : ""}
-        >
-          Volta Account
-        </Link>
-        <div
-          className="header__menu"
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-        >
-          {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div className="header">
-      <div className="header__logo">
-        <Link to="/">
-          <img src={window.voltaLogoURL} alt="Volta Logo" />
-        </Link>
-      </div>
+        {currentUser ? (
+          <Link
+            to="/userProfile"
+            className={isMenuOpen ? "header__link--hidden" : ""}
+          >
+            Volta Account
+          </Link>
+        ) : (
+          <Link
+            to="/login"
+            className={isMenuOpen ? "header__link--hidden" : ""}
+          >
+            Volta Account
+          </Link>
+        )}
 
-      <div className="header__links">
-        <Link to="/">Model S</Link>
-        <Link to="/">Model 3</Link>
-        <Link to="/">Model X</Link>
-        <Link to="/">Model Y</Link>
-        <Link to="/">Solar Roof</Link>
-        <Link to="/">Solar Panels</Link>
-      </div>
-
-      <div className="header__right">
-        <Link to="/" className={isMenuOpen ? "header__link--hidden" : ""}>
-          Shop
-        </Link>
-        <Link to="/login" className={isMenuOpen ? "header__link--hidden" : ""}>
-          Volta Account
-        </Link>
         <div
           className="header__menu"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
