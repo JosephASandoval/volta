ca283edcb27892bdbce504e690db52ff0e951ac5
JosephASandoval <josephasandoval17@gmail.com>
2021-02-16 15:41:33 -0800

    Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@
 #
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password_digest, presence: true
