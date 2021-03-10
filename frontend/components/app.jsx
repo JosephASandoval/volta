3e4ed1e329d94a2830a5c9ef1a7bfba915ab7176
JosephASandoval <josephasandoval17@gmail.com>
2021-03-09 16:05:28 -0800

    deleted test components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,8 +8,6 @@ import UserProfileContainer from "./user_profile/user_profile_container";
 import SignupContainer from "./session/signup_container";
 import LoginContainer from "./session/login_container";
 import NotFoundContainer from "./notfound/notfound_container";
-// import ProductIndexContainer from "./products/product_index_container"
-// import ProductShowContainer from "./products/product_show_container";
 import CarContainer from "./cars/car_container"
 
 const App = () => {
@@ -28,23 +26,11 @@ const App = () => {
           component={UserProfileContainer}
         />
 
-        {/* <Route
-          exact
-          path="/products"
-          component={ProductIndexContainer}
-        /> */}
-
         <Route
           exact
           path="/cars"
           component={CarContainer}
         />
-
-        {/* <Route
-          exact
-          path="/products/:productId"
-          component={ProductShowContainer}
-        /> */}
 
         <Route path="*" component={NotFoundContainer} />
       </Switch>
